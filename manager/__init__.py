2f259b8ba37b7f96736274310d919fa53ee36459
Tanuj2552 <tanujtvs2552@gmail.com>
2021-07-24 20:43:01 +0530

    changed init py file for creating app and blueprint

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,7 +1,26 @@
+import os
+
 from flask import Flask, render_template
 
-app = Flask("manager")
+def create_app(test_config = None):
+    app = Flask("manager")
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, 'manager.sqlite')
+    )
 
-@app.route('/')
-def start():
-    return render_template("index.html")
+    if test_config is not None:
+        app.config.update(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    from . import lister
+    app.register_blueprint(lister.bp)
+
+    from . import db 
+    db.init_app(app) 
+
+
+    return app
