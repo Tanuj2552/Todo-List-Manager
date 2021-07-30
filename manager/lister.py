3b4d8ceee0a0f32f06218356e87a3c59fbdd5ea5
Tanuj2552 <tanujtvs2552@gmail.com>
2021-07-30 22:23:16 +0530

    modified lister for managing the homepage and take new task from user and add it to the database, only title and description, need to work on date input

--- manager/lister.py
+++ manager/lister.py
@@ -8,17 +8,65 @@ from . import db
 
 bp = Blueprint("lister", "lister", url_prefix="")
 
+def get_date(d):
+    return d.date()
+
+def get_time(d):
+    time = d.strftime("%H: %M")
+    return time
+
 @bp.route("/")
 def dashboard():
     conn = db.get_db()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM Tasks")
-    date, title, description = cur.fetchall()[0]
+    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
+    day, time, title, description = cur.fetchall()[0]
     print("They are: ")
-    print(date, title, description)
-    cur.execute("SELECT * FROM Tasks")
+    print(day, time,  title, description)
+    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
     values = cur.fetchall()
     print("And values are: ")
     print(values)
 
     return render_template('index.html', values = values)
+
+@bp.route("/homepage")
+def home_page():
+    conn = db.get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
+    day, time, title, description = cur.fetchall()[0]
+    print("They are: ")
+    print(day, time,  title, description)
+    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
+    values = cur.fetchall()
+    print("And values are: ")
+    print(values)
+
+    cur.close()
+    conn.commit()
+    db.close_db()
+
+    return render_template('index.html', values = values)
+
+@bp.route("/add_task", methods=["GET", "POST"])
+def AddTask():
+    conn = db.get_db()
+    cursor = conn.cursor()
+    if request.method == "GET":
+      
+        return render_template("add_task.html")
+
+    elif request.method == "POST":
+        description = request.form.get('Description')
+        Title = request.form.get('Title')
+        done = 0
+        cursor.execute("INSERT INTO Tasks (Title, Description, Done) VALUES (?,?,?)",[Title, description, done])
+        print("These inserted")
+        cursor.close()
+        conn.commit()
+        db.close_db()
+
+
+        # TODO Handle sold
+        return redirect(url_for("lister.home_page"))
