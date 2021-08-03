89e943ad8208b088055212058a8d0fe48547395a
Tanuj2552 <tanujtvs2552@gmail.com>
2021-08-03 22:16:56 +0530

    Modified lister file to handle edit tasks functionality and smoothly handle week tasks and all

--- manager/lister.py
+++ manager/lister.py
@@ -31,11 +31,14 @@ def format_date(d):
 def dashboard():
     conn = db.get_db()
     cur = conn.cursor()
-    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
-    day, time, title, description = cur.fetchall()[0]
-    print("They are: ")
-    print(day, time,  title, description)
-    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
+   # cur.execute("SELECT id, task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
+   # id, day, time, title, description = cur.fetchall()[0]
+   # print("They are: ")
+   # print(id, day, time,  title, description)
+    everything = cur.execute("SELECT * FROM Tasks").fetchall()
+    print("everything is ", everything)
+
+    cur.execute("SELECT id, task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
     values = cur.fetchall()
     print("And values are: ")
     print(values)
@@ -49,11 +52,11 @@ def dashboard():
 def home_page():
     conn = db.get_db()
     cur = conn.cursor()
-    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
-    day, time, title, description = cur.fetchall()[0]
+    cur.execute("SELECT id, task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
+    id, day, time, title, description = cur.fetchall()[0]
     print("They are: ")
-    print(day, time,  title, description)
-    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
+    print(id, day, time,  title, description)
+    cur.execute("SELECT id, task_date, task_time, Title, Description FROM Tasks WHERE Done = ?",[0])
     values = cur.fetchall()
     print("And values are: ")
     print(values)
@@ -96,7 +99,7 @@ def Today_tasks():
     # day, time, title, description = cur.fetchall()[0]
     # print("They are: ")
     # print(day, time,  title, description)
-    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ? AND task_date = ?",[0, today_date])
+    cur.execute("SELECT id, task_date, task_time, Title, Description FROM Tasks WHERE Done = ? AND task_date = ?",[0, today_date])
     values = cur.fetchall()
     print("And values are: ")
     print(values)
@@ -122,7 +125,7 @@ def Week_tasks():
     day_of_week = today.isocalendar()[2] - 1
     rest_of_week = dates[day_of_week:]
     
-    cur.execute("SELECT task_date, task_time, Title, Description FROM Tasks WHERE Done = ? ",[0,])
+    cur.execute("SELECT id, task_date, task_time, Title, Description FROM Tasks WHERE Done = ? ",[0,])
     values = cur.fetchall()
     
     values = list(filter(lambda x: x[0] in rest_of_week, values))
@@ -131,3 +134,37 @@ def Week_tasks():
     db.close_db()
 
     return render_template('index.html', values = values)
+
+@bp.route("/<id>/edit_task", methods=["GET", "POST"])
+def Edit_tasks(id):
+    conn = db.get_db()
+    cursor = conn.cursor()
+    values = cursor.execute("SELECT id, Title, Description, original_task_time FROM Tasks WHERE id = ? ",[id,])
+    values = cursor.fetchall()
+    date_goog_init = cursor.execute("SELECT original_task_time FROM Tasks WHERE id = ?", [id]).fetchall()[0]
+    print("inti google date is: ", date_goog_init)
+    if request.method == "GET":
+        
+        return render_template("edit_task.html", values = values)
+
+    elif request.method == "POST":
+        description = request.form.get('Description')
+        Title = request.form.get('Title')
+        # done = 0
+        date_goog = request.form.get('birthdaytime')
+        print("received date_goog is ", date_goog)
+        if(date_goog is None) or len(date_goog) < 2:
+            date_goog = date_goog_init[0]
+        #date_goog = date_goog_init[0]
+        date = get_date(date_goog[:10])
+        time = get_time(date_goog[11:])
+        cursor.execute("UPDATE Tasks SET description = ?, Title = ?, task_date = ?, task_time = ?, original_task_time=? WHERE id = ?",[description, Title, date, time, date_goog, id])
+        print("These inserted")
+        cursor.close()
+        conn.commit()
+        db.close_db()
+        
+        return redirect(url_for("lister.home_page"))
+
+
+
